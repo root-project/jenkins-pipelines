6ec2caf9cc9a69e3d2e05f85544d5914c181d946
Martin Storø Nyfløtt <martin_mine@hotmail.com>
2017-04-19 10:43:01 +0200

    Typo

--- src/cern/root/pipeline/GenericBuild.groovy
+++ src/cern/root/pipeline/GenericBuild.groovy
@@ -135,13 +135,13 @@ class GenericBuild implements Serializable {
 
         def result = template.make(binding).toString()
 
-        def recpipients = 'martin.storo.nyflott@cern.ch'
+        def recipients = 'martin.storo.nyflott@cern.ch'
 
         script.emailext(
                 body: result, mimeType: 'text/html',
                 //recipientProviders:
                 //        [[$class: 'CulpritsRecipientProvider'], [$class: 'DevelopersRecipientProvider']],
                 replyTo: '$DEFAULT_REPLYTO', subject: '$DEFAULT_SUBJECT',
-                to: recpipients)
+                to: recipients)
     }
 }
