e95298def3bf1ebbba21af0199f611b50356b484
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-25 10:52:55 +0200

    Don't run the tests on Windows

--- GenericBuild.groovy
+++ GenericBuild.groovy
@@ -59,22 +59,24 @@ node(LABEL) {
                 }
             }
 
-            stage('Test') {
-                sh 'rootspi/jenkins/jk-all test'
+            if (LABEL != 'windows10') {
+                stage('Test') {
+                    sh 'rootspi/jenkins/jk-all test'
 
-                def testThreshold = [[$class: 'FailedThreshold', 
-                        failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0', 
-                        unstableThreshold: '0'], [$class: 'SkippedThreshold', failureNewThreshold: '', 
-                        failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']]
+                    def testThreshold = [[$class: 'FailedThreshold',
+                            failureNewThreshold: '0', failureThreshold: '0', unstableNewThreshold: '0',
+                            unstableThreshold: '0'], [$class: 'SkippedThreshold', failureNewThreshold: '',
+                            failureThreshold: '', unstableNewThreshold: '', unstableThreshold: '']]
 
-                step([$class: 'XUnitBuilder', 
-                        testTimeMargin: '3000', thresholdMode: 1, thresholds: testThreshold, 
-                        tools: [[$class: 'CTestType', 
-                                deleteOutputFiles: true, failIfNotNew: false, pattern: 'build/Testing/*/Test.xml', 
-                                skipNoTestFiles: false, stopProcessingIfError: true]]])
-                
-                if (currentBuild.result == 'FAILURE') {
-                    throw new Exception("Test result caused build to fail")
+                    step([$class: 'XUnitBuilder',
+                            testTimeMargin: '3000', thresholdMode: 1, thresholds: testThreshold,
+                            tools: [[$class: 'CTestType',
+                                    deleteOutputFiles: true, failIfNotNew: false, pattern: 'build/Testing/*/Test.xml',
+                                    skipNoTestFiles: false, stopProcessingIfError: true]]])
+
+                    if (currentBuild.result == 'FAILURE') {
+                        throw new Exception("Test result caused build to fail")
+                    }
                 }
             }
         } catch (err) {
