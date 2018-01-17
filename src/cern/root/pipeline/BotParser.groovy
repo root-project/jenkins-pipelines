84bc95d0b6685d687ed6d769f20c969e256e0ddd
Guilherme Amadio <amadio@cern.ch>
2018-01-17 15:57:49 +0100

    Revert "Add rule to specify externals in PR builds"
    This reverts commit 320c5fab04e33d2f33c0e9f78f35c78f701e8b18.

--- src/cern/root/pipeline/BotParser.groovy
+++ src/cern/root/pipeline/BotParser.groovy
@@ -11,8 +11,7 @@ class BotParser implements Serializable {
     private boolean parsableComment
     private String matrix
     private String flags
-    private String externals
-    private static final String COMMENT_REGEX = 'build (((?<overrideDefaultConfiguration>just|also)\\s)?on (?<matrix>([a-z0-9_]*\\/[a-z0-9_]*,?\\s?)*))?(with externals <externals>([a-z0-9_]*)\\s)?(with flags (?<flags>.*))?'
+    private static final String COMMENT_REGEX = 'build (((?<overrideDefaultConfiguration>just|also)\\s)?on (?<matrix>([a-z0-9_]*\\/[a-z0-9_]*,?\\s?)*))?(with flags (?<flags>.*))?'
     private def script
 
     /**
@@ -86,7 +85,6 @@ class BotParser implements Serializable {
             overrideDefaultConfiguration = matcher.group('overrideDefaultConfiguration').equals('just')
             matrix = matcher.group('matrix')
             flags = matcher.group('flags')
-            externals = matcher.group('externals')
 
             // If we specify a build configuration without "also/just on ...", only build on those platforms
             if (matrix != null && matcher.group('overrideDefaultConfiguration') == null) {
@@ -112,7 +110,7 @@ class BotParser implements Serializable {
                 def patternArgs = unparsedPattern.split('/')
                 def platform = patternArgs[0]
                 def compiler = patternArgs[1]
-
+                
 
                 script.println "Received label $platform with compiler $compiler"
 
@@ -132,14 +130,9 @@ class BotParser implements Serializable {
             extraCMakeOptions = cmakeFlagsMap.collect { /$it.key=$it.value/ } join ' '
         }
 
-        if (externals == null) {
-            externals = "ROOT-latest"
-        }
-
         script.println "ExtraCMakeOptions set to $extraCMakeOptions"
         script.println "Add default matrix config: $overrideDefaultConfiguration"
         script.println "CMake flags: $flags"
-        script.println "Externals: $externals"
     }
 
     /**
@@ -203,6 +196,5 @@ class BotParser implements Serializable {
         }
 
         build.addBuildParameter('ExtraCMakeOptions', extraCMakeOptions)
-        build.addBuildParameter('EXTERNALS', externals)
     }
 }
