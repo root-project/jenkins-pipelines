dcf4ba7f1d2c2a040929f21bb759aa5d9f8e04e2
Axel Naumann <Axel.Naumann@cern.ch>
2018-07-30 13:40:00 +0200

    Jenkins pipelines: increase timeout to 10 mins for NTFS.

--- GenericBuild.groovy
+++ GenericBuild.groovy
@@ -33,7 +33,7 @@ node(LABEL) {
                     // TODO: Use the git step when it has implemented specifying refspecs
                     // See https://jenkins.io/doc/pipeline/steps/workflow-scm-step/ for CloneOption
                     checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false,
-                            extensions: [[$class: 'CloneOption', timeout: 5, noTags: true]],
+                            extensions: [[$class: 'CloneOption', timeout: 10, noTags: true]],
                             submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
                 }
             }
@@ -44,7 +44,7 @@ node(LABEL) {
                         def rootTestUrl = 'https://github.com/root-project/roottest.git';
                         // TODO: Use the git step when it has implemented specifying refspecs
                         checkout([$class: 'GitSCM', branches: [[name: ROOTTEST_BRANCH]], doGenerateSubmoduleConfigurations: false,
-                                extensions: [[$class: 'CloneOption', timeout: 5, noTags: true]],
+                                extensions: [[$class: 'CloneOption', timeout: 10, noTags: true]],
                                 submoduleCfg: [], userRemoteConfigs: [[refspec: ROOTTEST_REFSPEC, url: rootTestUrl]]])
                     }
                 }
