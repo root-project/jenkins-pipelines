a5b5667b2f40b33e2ba52f46d66c8316428bcea8
Axel Naumann <Axel.Naumann@cern.ch>
2018-08-01 11:30:21 +0200

    Jenkins pipelines: force non-shallow clones.

--- GenericBuild.groovy
+++ GenericBuild.groovy
@@ -33,7 +33,7 @@ node(LABEL) {
                     // TODO: Use the git step when it has implemented specifying refspecs
                     // See https://jenkins.io/doc/pipeline/steps/workflow-scm-step/ for CloneOption
                     checkout([$class: 'GitSCM', branches: [[name: ROOT_BRANCH]], doGenerateSubmoduleConfigurations: false,
-                            extensions: [[$class: 'CloneOption', timeout: 10, noTags: true]],
+                            extensions: [[$class: 'CloneOption', timeout: 10, noTags: true, shallow: false]],
                             submoduleCfg: [], userRemoteConfigs: [[refspec: ROOT_REFSPEC, url: env.GIT_URL]]])
                 }
             }
@@ -44,7 +44,7 @@ node(LABEL) {
                         def rootTestUrl = 'http://root.cern/git/roottest.git';
                         // TODO: Use the git step when it has implemented specifying refspecs
                         checkout([$class: 'GitSCM', branches: [[name: ROOTTEST_BRANCH]], doGenerateSubmoduleConfigurations: false,
-                                extensions: [[$class: 'CloneOption', timeout: 10, noTags: true]],
+                                extensions: [[$class: 'CloneOption', timeout: 10, noTags: true, shallow: false]],
                                 submoduleCfg: [], userRemoteConfigs: [[refspec: ROOTTEST_REFSPEC, url: rootTestUrl]]])
                     }
                 }
