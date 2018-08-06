779ef910c89cb936a1ee3235f61305799b52f484
Guilherme Amadio <amadio@cern.ch>
2018-08-06 10:58:54 +0200

    Set default of ExtraCMakeOptions to -Dccache=ON only, as imt is already default

--- Pullrequests.groovy
+++ Pullrequests.groovy
@@ -17,7 +17,7 @@ properties([
         string(name: 'VERSION', defaultValue: 'master', description: 'Branch to be built'),
         string(name: 'EXTERNALS', defaultValue: 'ROOT-latest', description: ''),
         string(name: 'EMPTY_BINARY', defaultValue: 'true', description: 'Boolean to empty the binary directory (i.e. to force a full re-build)'),
-        string(name: 'ExtraCMakeOptions', defaultValue: '-Dimt=ON -Dccache=ON', description: 'Additional CMake configuration options of the form "-Doption1=value1 -Doption2=value2"'),
+        string(name: 'ExtraCMakeOptions', defaultValue: '-Dccache=ON', description: 'Additional CMake configuration options of the form "-Doption1=value1 -Doption2=value2"'),
         string(name: 'MODE', defaultValue: 'pullrequests', description: 'The build mode'),
         string(name: 'PARENT', defaultValue: 'root-pullrequests-trigger', description: 'Trigger job name')
     ])
