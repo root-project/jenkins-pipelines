9c9f64fc6037d85e2039dd347bdc16d2c30d0fea
Axel Naumann <Axel.Naumann@cern.ch>
2019-10-28 14:40:21 +0100

    Re-enable 32bit linux, add cxx14 build.

--- src/cern/root/pipeline/BuildConfiguration.groovy
+++ src/cern/root/pipeline/BuildConfiguration.groovy
@@ -58,8 +58,9 @@ class BuildConfiguration {
             [ label: 'ROOT-performance-centos7-multicore', opts: extraCMakeOptions + ' -DCTEST_TEST_EXCLUDE_NONE=On', spec: 'default' ],
             [ label: 'ROOT-fedora27',  opts: extraCMakeOptions, spec: 'noimt' ],
             [ label: 'ROOT-fedora29',  opts: extraCMakeOptions, spec: 'python3' ],
+            [ label: 'ROOT-fedora30',  opts: extraCMakeOptions, spec: 'cxx14' ],
             [ label: 'ROOT-ubuntu16',  opts: extraCMakeOptions, spec: 'rtcxxmod' ],
-            /*[ label: 'ROOT-ubuntu18.04-i386',  opts: extraCMakeOptions, spec: 'cxx14' ],*/
+            [ label: 'ROOT-ubuntu18.04-i386',  opts: extraCMakeOptions, spec: 'cxx14' ],
             [ label: 'mac1014',   opts: extraCMakeOptions, spec: 'cxx17' ],
             [ label: 'windows10', opts: extraCMakeOptions, spec: 'default' ]
         ]
