cbdb27b8ed9bf9a7628d17e8542b0a650e447448
Vassil Vassilev <v.g.vassilev@gmail.com>
2020-04-04 14:09:26 +0300

    Add the current date to the link.

--- src/cern/root/pipeline/GitHub.groovy
+++ src/cern/root/pipeline/GitHub.groovy
@@ -86,8 +86,9 @@ class GitHub implements Serializable {
     void postResultComment(buildWrapper) {
         def commentBuilder = new StringBuilder()
         def buildUrl = Jenkins.activeInstance.rootUrl + buildWrapper.result.rawBuild.url
+        def today = new Date().format("yyyy-MM-dd");
         commentBuilder.append("Build failed on ${buildWrapper.label}/${buildWrapper.spec}.\n")
-        commentBuilder.append("[See cdash ](http://cdash.cern.ch/index.php?project=ROOT&filtercount=1&field1=buildname/string&compare1=65&value1=PR-${prId})")
+        commentBuilder.append("[See cdash ](http://cdash.cern.ch/index.php?project=ROOT&filtercount=1&field1=buildname/string&compare1=65&value1=PR-${prId}&date=${today}).\n")
         commentBuilder.append("[See console output](${buildUrl}console).\n")
         
         def logParserAction = buildWrapper.result.rawBuild.getAction(LogParserAction.class)
