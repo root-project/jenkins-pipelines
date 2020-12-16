d6d9f1e6f85f97a0a35b062bf94ff5f9db842f30
Axel Naumann <Axel.Naumann@cern.ch>
2020-12-16 10:29:18 +0100

    Temporarily disable broken C8-multicore. Bring mac10.14 back; move 1015 to 11.0; fed31 to 32.

--- src/cern/root/pipeline/BuildConfiguration.groovy
+++ src/cern/root/pipeline/BuildConfiguration.groovy
@@ -63,12 +63,12 @@ class BuildConfiguration {
     static def getPullrequestConfiguration(extraCMakeOptions) {
         return [
             [ label: 'ROOT-debian10-i386',  opts: extraCMakeOptions, spec: 'cxx14' ],
-            [ label: 'ROOT-performance-centos8-multicore', opts: extraCMakeOptions + ' -DCTEST_TEST_EXCLUDE_NONE=On', spec: 'default' ],
+            /*[ label: 'ROOT-performance-centos8-multicore', opts: extraCMakeOptions + ' -DCTEST_TEST_EXCLUDE_NONE=On', spec: 'default' ],*/
             [ label: 'ROOT-fedora30',  opts: extraCMakeOptions, spec: 'cxx14' ],
-            [ label: 'ROOT-fedora31',  opts: extraCMakeOptions, spec: 'noimt' ],
+            [ label: 'ROOT-fedora32',  opts: extraCMakeOptions, spec: 'noimt' ],
             [ label: 'ROOT-ubuntu16',  opts: extraCMakeOptions, spec: 'nortcxxmod' ],
-/* Running out of 10.14 machines: [ label: 'mac1014',   opts: extraCMakeOptions, spec: 'python3' ],*/
-            [ label: 'mac1015',   opts: extraCMakeOptions, spec: 'cxx17' ],
+            [ label: 'mac1014',   opts: extraCMakeOptions, spec: 'python3' ],
+            [ label: 'mac11.0',   opts: extraCMakeOptions, spec: 'cxx17' ],
             [ label: 'windows10', opts: extraCMakeOptions, spec: 'cxx14' ]
         ]
     }
