274f5ab8c935c177dabc638af51b54fc6a8d6338
Axel Naumann <Axel.Naumann@cern.ch>
2021-05-27 17:48:54 +0200

    Fed32 still has 3 failures.

--- src/cern/root/pipeline/BuildConfiguration.groovy
+++ src/cern/root/pipeline/BuildConfiguration.groovy
@@ -65,7 +65,6 @@ class BuildConfiguration {
         return [
             [ label: 'ROOT-debian10-i386',  opts: extraCMakeOptions, spec: 'cxx14' ],
             [ label: 'ROOT-performance-centos8-multicore', opts: extraCMakeOptions + ' -DCTEST_TEST_EXCLUDE_NONE=On', spec: 'default' ],
-            [ label: 'ROOT-fedora32',  opts: extraCMakeOptions, spec: 'noimt' ],
             [ label: 'ROOT-ubuntu16',  opts: extraCMakeOptions, spec: 'nortcxxmod' ],
             [ label: 'mac1014',   opts: extraCMakeOptions, spec: 'python3' ],
             [ label: 'mac11.0',   opts: extraCMakeOptions + ' -DCTEST_TEST_EXCLUDE_NONE=On', spec: 'cxx17' ],
